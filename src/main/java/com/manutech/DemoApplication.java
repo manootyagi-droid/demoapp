b3caed081ad9540e16123a0ec2abdc804dd02337
Manu Tyagi <manootyagi@gmail.com>
2026-02-26 11:15:30 +0000

    first commit

---
+++ src/main/java/com/manutech/DemoApplication.java
@@ -0,0 +1,19 @@
+package com.manutech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+
+    @GetMapping("/")
+    public String home() {
+        return "Hello Manu 🚀 CI/CD Working!";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+}
