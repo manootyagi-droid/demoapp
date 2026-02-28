45224008fbbc8d71f4e96d54f833a7388ad8b167
manootyagi-droid <manootyagi@gmail.com>
2026-02-28 11:57:27 +0530

    Update DemoApplication.java

--- src/main/java/com/manutech/DemoApplication.java
+++ src/main/java/com/manutech/DemoApplication.java
@@ -10,7 +10,7 @@ public class DemoApplication {
 
     @GetMapping("/")
     public String home() {
-        return "Hello Manu Tyagi 🚀 CI/CD Working!";
+        return "Hello Manu Tyagi g 🚀 CI/CD Working!";
     }
 
     public static void main(String[] args) {
